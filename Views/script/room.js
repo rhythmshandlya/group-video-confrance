c80ea81021bafdb5db67c0e90c211aa9610af5f3
rhythmshandlya <armaanbgp@gmail.com>
2021-10-11 22:40:04 +0530

    d

--- Views/script/room.js
+++ Views/script/room.js
@@ -1,4 +1,8 @@
 const socket = io('/');
+const videoGrid = document.getElementById('room-grid');
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
 
 var peer = new Peer(undefined, {
   path: '/peerjs',
@@ -6,17 +10,64 @@ var peer = new Peer(undefined, {
   port: 3000
 });
 
-peer.on('open', function (id) {
-  console.log('My peer ID is: ' + id);
-  socket.emit('join-room', roomId, id);
+//When peer connection is established
+peer.on('open', (MyId) => {
+  console.log('My id ', MyId);
+  socket.emit('join-room', roomId, MyId);
 });
 
-socket.on('new-user-connected', (id) => {
-  const newP = document.createElement('p');
-  newP.innerHTML = 'A new user connected';
-  newP.classList.add('center');
-  document.getElementsByClassName('chat-data')[0].appendChild(newP);
-});
+const connectToNewUser = (new_user_id, stream) => {
+  const call = peer.call(new_user_id, stream);
+  const video = document.createElement('video');
+  console.log(call.on);
+  call.on('stream', (usersStream) => {
+    connectVideoStreamAndAppend(video, usersStream);
+  });
+  call.on('close', () => {
+    video.remove();
+  });
+};
+
+const connectVideoStreamAndAppend = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true
+  })
+  .then((stream) => {
+    connectVideoStreamAndAppend(myVideo, stream);
+
+    socket.on('new-user-connected', async (new_user_id) => {
+      console.log('New user connected ', new_user_id);
+
+      //Connect Video
+      connectToNewUser(new_user_id, stream);
+
+      //Display message in chat
+      const newP = document.createElement('p');
+      newP.innerHTML = 'A new user connected';
+      newP.classList.add('center');
+      document.getElementsByClassName('chat-data')[0].appendChild(newP);
+    });
+
+    peer.on('call', (call) => {
+      const newVideo = document.createElement('video');
+      call.answer(stream);
+      call.on('stream', (usersStream) => {
+        connectVideoStreamAndAppend(newVideo, usersStream);
+      });
+    });
+  })
+  .catch((err) => {
+    alert(err);
+  });
 
 socket.on('deliver-message', (message, id) => {
   const newP = document.createElement('p');
